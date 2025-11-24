7604b17f87d0e830a0ab0df0dd405d4a79a2fed7
rezaaryandi <rezaaryandi.id@gmail.com>
2025-11-24 11:53:46 +0700

    h

--- bersih.c
+++ bersih.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
-int main(){
- 
+#include <string.h>
+#include <ctype.h>
 
+ int main(){
+    typedef struct{ 
+        char title[10000];
+        char kalimat[100000];
+      
+    } tipe;
+
+    tipe data;
+    FILE *input_file, *hasil;
+    input_file = fopen ("datamentah.txt", "r");
+    if (input_file == NULL){
+        printf("file gak bisa dibuka");
+        return 1;
+    }
+    hasil = fopen ("hasil.txt", "w");
+      if (hasil == NULL){
+         printf("file gak bisa dibuka");
+         return 1;
+      }
+    
+    while (fscanf(input_file," <url>%*[^<]</url><title>%9999[^<]</title><body>%99999[^<]</body> ", data.title, data.kalimat) == 2){ 
+        fprintf(hasil, "%s %s\n", data.title, data.kalimat); 
+        hasil = toloweCase(hasil);
+    }
+
+fclose(hasil);
+fclose(input_file);
 
     return 0;
-}
+ }
